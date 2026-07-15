5d606688447af871171789d963b73e898f5eb0c5
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-07-15 15:05:16 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for(int a : asteroids){
+            if(a > 0){
+                st.push(a);
+            } else if(a < 0){
+                int top = st.peek();
+                if(top == Math.abs(a)){
+                    st.pop();
+                }
+            }
+        }
+
+        int size = st.size();
+        int[] res = new int[size];
+        int i = 0;
+        for(int a : st){
+            res[i++] = a;
+        }
+        return res;
+    }
+}
