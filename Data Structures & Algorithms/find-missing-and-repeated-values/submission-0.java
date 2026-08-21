a1b8b857ff9bbb42a0a2cb11f3c9d68665369f50
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-08-22 00:40:08 +0530

    Add: find-missing-and-repeated-values - submission-0

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        Set<Integer> set = new HashSet<>();
+        int n = grid.length;
+        int[] res = new int[2];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(set.contains(grid[i][j])){
+                    res[0] = grid[i][j];
+                    continue;
+                }
+                set.add(grid[i][j]);
+            }
+        }
+
+        for(int i = 1; i <= n*n; i++){
+            if(!set.contains(i)){
+                res[1] = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
