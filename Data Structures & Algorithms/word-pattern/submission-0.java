fd6d3a6f47c08d85ba2291bf157b771ab40798c5
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-08-23 01:02:11 +0530

    Add: word-pattern - submission-0

---
+++ Data Structures & Algorithms/word-pattern/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> hm = new HashMap<>();
+        String[] words = s.split(" ");
+        for(int i = 0; i < pattern.length(); i++){
+            char p = pattern.charAt(i);
+            hm.put(p,words[i]);
+        }
+
+        for(int i = 0; i < pattern.length(); i++){
+            char p = pattern.charAt(i);
+            if(!hm.get(p).equals(words[i])) return false;
+        }
+
+        return true;
+        
+    }
+}
