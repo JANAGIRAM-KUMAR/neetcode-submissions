a5dbd30009ed8e8addabb0a179feb71b34c4bfbc
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-08-29 23:34:35 +0530

    Add: matchsticks-to-square - submission-2

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[] sides = new int[4];
+    int totalSum = 0;
+    int[] matchsticks;
+    int sideLen;
+    public boolean makesquare(int[] matchsticks) {    
+        this.matchsticks = matchsticks;  
+        for(int m : matchsticks){
+            totalSum +=  m;
+        }
+        sideLen = totalSum / 4; 
+        if(totalSum % 4 != 0) return false;
+        return dfs(0);
+         
+    }
+    public boolean dfs(int idx){
+        if(idx == sideLen){
+            return true;
+        }
+        for(int i = 0; i < 4; i++){
+            if(sides[i] + matchsticks[idx] <= sideLen){
+                sides[i] += matchsticks[idx];
+                if(dfs(idx+1)) return true;
+                sides[i] -= matchsticks[idx];
+            }
+        }
+        return false;
+    }
+}
