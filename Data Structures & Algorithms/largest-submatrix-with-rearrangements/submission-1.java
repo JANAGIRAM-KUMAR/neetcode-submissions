95314ef339ae8d62cc315b87d8d96b81fdb1341b
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-09-07 09:30:25 +0530

    Add: largest-submatrix-with-rearrangements - submission-1

---
+++ Data Structures & Algorithms/largest-submatrix-with-rearrangements/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] heights = new int[n];
+        int ans = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 1) heights[j]++;
+                else heights[j] = 0;
+            }
+            int[] sorted = heights.clone();
+            Arrays.sort(sorted);
+            for(int j = 0; j < n; j++){
+                int h = sorted[n-1-j];
+                int w = j+1;
+                ans = Math.max(ans, h * w);
+            }
+        }
+
+        return ans;
+        
+    }   
+}
