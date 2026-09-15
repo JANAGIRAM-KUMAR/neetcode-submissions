30dde22fbdbe03f831393f07ef46192684169776
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-09-15 22:45:03 +0530

    Add: valid-perfect-square - submission-6

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long n = sqrt(num);
+        return n*n == num;
+    }
+
+    public long sqrt(int num){
+        long s = 0;
+        long e = num;
+        long res = 0;
+        while(s <= e){
+            long m = s + (e-s) / 2;
+            if(m * m == num) return m;
+            else if(m * m < num){
+                res = m;
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return res;
+    }
+}
