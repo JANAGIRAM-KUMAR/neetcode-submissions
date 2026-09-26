22cdeb22fbfc1a87a4b942368ad009503638a384
JANAGIRAM KUMAR <148270509+JANAGIRAM-KUMAR@users.noreply.github.com>
2026-09-26 22:56:58 +0530

    Add: frequency-of-the-most-frequent-element - submission-4

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        int l = 0;
+        int currSum = 0;
+        int maxFreq = 0;
+
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int r = 0; r < n; r++){
+            currSum += nums[r];
+            if(nums[r] * (r-l+1) > currSum + k){
+                currSum -= nums[l];
+                l++;
+            }
+            maxFreq = Math.max(maxFreq,r-l+1);
+        }
+        return maxFreq;
+    }
+}
+
